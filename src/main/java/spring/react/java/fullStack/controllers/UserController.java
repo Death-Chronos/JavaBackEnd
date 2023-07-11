980998cc6a09ec3472efdab4b70e69d83d7b3aa1
João Victor Costa <80175567+Death-Chronos@users.noreply.github.com>
2023-07-11 15:14:56 -0300

    GetALL adicionado

--- src/main/java/spring/react/java/fullStack/controllers/UserController.java
+++ src/main/java/spring/react/java/fullStack/controllers/UserController.java
@@ -1,21 +1,36 @@
 package spring.react.java.fullStack.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
+import jakarta.validation.Valid;
+import spring.react.java.fullStack.DTOs.UserDTO;
 import spring.react.java.fullStack.models.User;
 import spring.react.java.fullStack.repositorys.UserRepository;
 
 @RestController
-public class UserController{
+public class UserController {
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("/user")
-    User newUser(@RequestBody User newUser){
-        return userRepository.save(newUser);
+    ResponseEntity<User> newUser(@RequestBody @Valid UserDTO newUserDTO) {
+        User user = new User();
+        BeanUtils.copyProperties(newUserDTO, user); // Faz a transformação DTO para Usuario
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
+    }
+    @GetMapping("/users")
+    ResponseEntity<List<User>> getAllUsers (){
+        return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
     }
 }
+
