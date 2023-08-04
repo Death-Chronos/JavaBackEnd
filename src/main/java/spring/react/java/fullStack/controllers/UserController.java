e281685b6a3485fd9936e0bfa669d7a748c90084
João Victor Costa <80175567+Death-Chronos@users.noreply.github.com>
2023-08-04 17:33:05 -0300

    Pegar apenas um usuario

--- src/main/java/spring/react/java/fullStack/controllers/UserController.java
+++ src/main/java/spring/react/java/fullStack/controllers/UserController.java
@@ -1,6 +1,7 @@
 package spring.react.java.fullStack.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,9 @@ import jakarta.validation.Valid;
 import spring.react.java.fullStack.DTOs.UserDTO;
 import spring.react.java.fullStack.models.User;
 import spring.react.java.fullStack.repositorys.UserRepository;
+import spring.react.java.fullStack.exceptions.UserNotFoundException;
 
-@RestController 
+@RestController
 @CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
@@ -27,12 +30,21 @@ public class UserController {
     ResponseEntity<User> newUser(@RequestBody @Valid UserDTO newUserDTO) {
         User user = new User();
         BeanUtils.copyProperties(newUserDTO, user); // Faz a transformação DTO para Usuario
-
         return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
     }
+
     @GetMapping("/users")
-    ResponseEntity<List<User>> getAllUsers (){
+    ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
     }
-}
 
+    @GetMapping("/user/{id}")
+    ResponseEntity<Object> getUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado"); // também podemos usar throw new UserNotFoundException(id);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(user.get());
+
+    }
+}
